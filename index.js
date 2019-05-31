41d98fa0371004d8af2e06357321dcde07666670
Ahnna <ahnnaostling@gmail.com>
2019-05-31 13:54:25 +0200

    add new port info

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import Database from './lib/db';
 import { cats, dogs, pokemons } from './creature';
- import Creature from './creatureClass';
+import Creature from './creatureClass';
 
 // Setup the server
 const PORT = 3000;
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 // // Test page to see console result
 // app.get('/console',(req, res)=>{
